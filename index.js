a033ab1cefe425ea2a2c0c840ff37c80b9d14bfb
Amanda Huang <aamandahuang@gmail.com>
2022-01-26 15:35:37 -0500

    commit tree f715b98617a64c99edf5327ff3d67496dc38f20e

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your code here!
+main.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.setAttribute('id','victory');
+newHeader.innerHTML= "YOUR-NAME is the champion";
+
+
+/*
+document.body.append(newHeader);
+newHeader.setAttribute('id','victory');
+
+newHeader.id = 'victory';
+newHeader.innerHTML = "YOUR-NAME is the champion";
+*/
